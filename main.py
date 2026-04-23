326e563cd9758660f1aac537cfc844e80aea0842
Chrysoula Kielland <chrysoula.kielland@gmail.com>
2026-04-23 11:40:16 +0200

    Added image CLI scan with image build fixing optional arguments remove sudo

--- main.py
+++ main.py
@@ -4,5 +4,9 @@
 from app import app
 
 if __name__ == '__main__':
+    # Adding secrets for PR Scanning
+    password = "admin123"
+    api_key = "AKIA1234567890"
+    secret = "hardcoded-secret"
     # Intentionally vulnerable: Running on all interfaces (CKV3_SAST_5)
     app.run(host='0.0.0.0', port=5000, debug=True)
